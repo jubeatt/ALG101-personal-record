60d71bf040394b20d3ecd36934758c870e26d10e
jim <jimdevelopesite@gmail.com>
2021-12-21 16:40:22 +0800

    init

---
+++ LIOJ/1001/index.js
@@ -0,0 +1,5 @@
+function solve(lines) {
+  const inputString = lines[0]  // '1 2'
+  const stringArray = inputString.split(' ')  // ['1', '2']
+  console.log(Number(stringArray[0]) + Number(stringArray[1])) // 3
+}
---
+++ README..md
@@ -0,0 +1,5 @@
+# \[ALG101\] 先別急著寫 leetcode 個人紀錄
+
+課程來源：[[ALG101]](https://github.com/Lidemy/ALG101-too-weak-to-leetcode)
+
+這個 repo 是用來放自己上課的練習題目，以及一些筆記用途。
