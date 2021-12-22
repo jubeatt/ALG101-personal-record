c006ad25f80623fac695c3b73c5884f524a0e4a3
jim <jimdevelopesite@gmail.com>
2021-12-23 03:29:44 +0800

    update example

--- example/1020/index.js
+++ example/1020/index.js
@@ -1,23 +1,16 @@
+// 函式結構寫法
 function solve(lines) {
   // 建立數字陣列
   let numberArray = transToNumber(lines.slice(1))
   // 遍歷所有數字
   numberArray.forEach((elem) => {
-    /*
-      edge case：
-      數字 1 不是質數也不是合數，但要輸出 Composite
-      數字 2 是特殊案例，所以直接輸出 Prime
-    */
-    if(elem === 1) console.log('Composite')
-    if(elem === 2) console.log('Prime')
-    if( elem===1 || elem===2 ) return 0
-
     // 判斷是否為質數
     let message = isPrime(elem) ? 'Prime' : 'Composite'
     // 印出輸出訊息
     console.log(message)
   })
 }
+// 陣列轉換成 Number
 function transToNumber (array) {
   let result = []
   for(let i=0; i<array.length; i++) {
@@ -26,6 +19,8 @@ function transToNumber (array) {
   return result
 }
 function isPrime (number) {
+  // edge case: 數字 1 不算質數
+  if(number === 1) return false
   // 遍歷 2 ~ n-1
   for(let i=2; i<number; i++) {
     // 可以整除就代表不是質數
@@ -33,4 +28,36 @@ function isPrime (number) {
   }
   // 都不能整除就代表是質數
   return true
+}
+
+
+
+
+
+// 雙層迴圈寫法
+function solve(lines) {
+  // 建立數字陣列
+  let numberArray = transToNumber(lines.slice(1))
+  // 遍歷所有數字
+  for(let i=0; i<numberArray.length; i++) {
+    // 預設為質數
+    let isPrime = true
+    // edge case: 1
+    if(numberArray[i] === 1) {
+      isPrime = false
+    }
+    // 判斷質數
+    for(let j=2; j<numberArray[i]; j++) {
+      if(numberArray[i] % j === 0) {
+        isPrime = false
+        break
+      }
+    }
+    // 印出結果
+    if(isPrime) {
+      console.log('Prime')
+    } else {
+      console.log('Composite')
+    }
+  }
 }
