8b80f606c553c9cad5ae73f827cd191de57574c0
jim <jimdevelopesite@gmail.com>
2021-12-27 17:17:49 +0800

    update

---
+++ example/1027/index.js
@@ -0,0 +1,86 @@
+// 解法一，內建函式
+function solve(lines) {
+  console.log(isValid(lines))
+}
+function isValid(array) {
+  // expected => [5,4,1,2,3, ...]
+  const numbers = array[0]
+                    .split('')
+                    .filter(value => value !== '-')
+                    .map(value => Number(value))
+  // 最後一個數字
+  const lastNum = numbers[numbers.length-1]
+  // 信用卡公司
+  const company = numbers[0] === 5 ? 'MASTER_CARD' : 'VISA'
+  // 消除最後一個數字
+  numbers.splice(-1, 1)
+  // 奇數項
+  const odd = numbers
+                .filter((value, index) => !(index & 1))
+                .map(value => value*2 >= 10 ? value*2-9 : value*2)
+                .reduce((acc, value) => acc + value, 0)
+  // 偶數項 
+  const even = numbers 
+                .filter((value, index) => index & 1)
+                .reduce((acc, value) => acc + value, 0)
+  // 檢查碼
+  const code = (odd+even) % 10 === 0 ? 0 : 10 - ((odd+even) % 10)
+  // 回傳訊息
+  return lastNum === code ? company : 'INVALID'
+}
+
+
+
+// 解法二，三寶解法
+function solve(lines) {
+  // [1, 2, 3, 4 ,5, ...]
+  const card = removeDash(lines[0].split(''))
+  // 信用卡公司
+  const company = card[0] === 5 ? 'MASTER_CARD' : 'VISA'
+  // 最後一個數字
+  const lastNum = card[15]
+  // 移除最後一個數字
+  const numbers = removeLastNum(card)
+  // 奇數總和
+  const odd = addOdds(numbers)
+  // 偶數總和
+  const even = addEvens(numbers)
+  // 檢查碼
+  const code = (odd+even)%10 === 0 ? 0 : 10 - ((odd+even)%10)
+  // 結果訊息
+  const message = lastNum === code ? company : 'INVALID'
+  // 印出結果
+  console.log(message)
+}
+function removeDash(arr) {
+  let result = []
+  for(let i=0; i<arr.length; i++) {
+    if(arr[i] !== '-') result.push(Number(arr[i]))
+  }
+  return result
+}
+function removeLastNum(arr) {
+  let result = []
+  for(let i=0; i<arr.length-1; i++) {
+    result.push(arr[i])
+  }
+  return result
+}
+function addOdds(arr) {
+  let sum = 0
+  for(let i=0; i<arr.length; i++) {
+    if( !(i & 1) ) {
+      arr[i]*2 >= 10 
+      ? sum += arr[i]*2 - 9 
+      : sum += arr[i]*2
+    }
+  }
+  return sum
+}
+function addEvens(arr) {
+  let sum = 0
+  for(let i=0; i<arr.length; i++) {
+    if(i & 1) sum += arr[i]
+  }
+  return sum
+}
---
+++ unit5/02/index.js
@@ -0,0 +1,38 @@
+function isValid(str) {
+  // 輸入規則：A-Z 開頭 + 9 個連續數字結尾
+  const isValidInput = /^[A-Z][0-9]{9}$/.test(str)
+  // 判斷輸入是否合法
+  if( !(isValidInput) ) return false
+  
+  // 字串
+  const IDString = codeTable(str[0]) + str.substring(1)
+  // 數字
+  const IDNumbers = IDString.split('').map(value => Number(value))
+  console.log('IDNumbers', IDNumbers)
+  // 公式表
+  const table = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1]
+  // 總和
+  let sum = 0
+  // 驗證計算
+  for(let i=0; i<IDNumbers.length; i++) {
+    sum += IDNumbers[i] * table[i]
+  }
+  // 回傳結果
+  console.log('sum:', sum)
+  return sum%10 === 0
+}
+// 字母對應表
+function codeTable(key) {
+  const map = {
+    A: '10', B: '11', C: '12',
+    D: '13', E: '14', F: '15',
+    G: '16', H: '17', I: '34',
+    J: '18', K: '19', L: '20',
+    M: '21', N: '22', O: '35',
+    P: '23', Q: '24', R: '25',
+    S: '26', T: '27', U: '28',
+    V: '29', W: '32', X: '30',
+    Y: '31', Z: '33'
+  }
+  return map[key]
+}
